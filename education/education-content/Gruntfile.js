d4e07e642ffe982186999ecccb6ee4611938f578
JamesWChan <jwchan1993@gmail.com>
2019-08-20 13:39:10 +0100

    Created education directory for landing and content pages

---
+++ education/education-content/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourcemap: false
+            },
+            dist: {
+                files: {
+                    'css/education-content.css': 'sass/base-sass.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/education-content.min.css': ['css/education-content.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
+
+};
